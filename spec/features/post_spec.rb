86687150fddce2665960de13e3469b4f35a3ec5e
Yohanan Assefa <nanahoy@gmail.com>
2017-06-19 16:28:13 -0400

    Implemented user connection with tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -14,20 +14,31 @@ describe 'navigate' do
   end
 
   describe "creation" do
-    it "has a new form that can be reached" do
+    before do
+      user = User.create(email: "test@test.com", password: "asdfasdf",
+                         password_confirmation: "asdfasdf", first_name: "John",
+                         last_name: "Snow")
+      login_as(user, :scope => :user)
       visit new_post_path
+    end
+    it "has a new form that can be reached" do
       expect(page.status_code).to eq(200)
     end
 
     it "can be created from new form page" do
-      visit new_post_path
-
       fill_in "post[date]", with: Date.today
       fill_in "post[rationale]", with: "Some rationale"
-
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
+    end
+
+    it "will have a user associated with it" do
+      fill_in "post[date]", with: Date.today
+      fill_in "post[rationale]", with: "User_Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User_Association")
     end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "creation" do
     before do
-      @post = Post.create(date: Date.today, rationale: "Anyting")
+      @user = User.create(email: "test@test.com", password: "asdfasdf",
+                         password_confirmation: "asdfasdf", first_name: "John",
+                         last_name: "Snow")
+      @post = Post.create(date: Date.today, rationale: "Anyting", user_id: @user.id)
     end
 
     it "can be created" do
