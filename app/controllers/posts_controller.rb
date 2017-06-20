955fb8d6bc9319cbbba03049a6f67842308bf1a8
Yohanan Assefa <nanahoy@gmail.com>
2017-06-20 12:26:52 -0400

    Refactored post page to use partial"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show]
 
   def index
+    @posts = Post.all
   end
 
   def new
@@ -10,7 +11,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.user_id = current_user.id 
+    @post.user_id = current_user.id
 
     if @post.save
       redirect_to @post, notice: "Your post was created successfully"
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+
+  def full_name(first_name, last_name) 
+    "#{first_name} #{last_name}"
+  end
 end
