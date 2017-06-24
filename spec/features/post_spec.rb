965ae7fdf4a031c07707dbe074e23f81667d88f1
Yohanan Assefa <nanahoy@gmail.com>
2017-06-23 18:18:05 -0600

    Refactored post_spec test

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  let(:user) { FactoryGirl.create(:user) }
+
+  let(:post) do
+    Post.create(date: Date.today, rationale: "rationale", user_id: user.id)
+  end
+
+
   before do
-    @user = FactoryGirl.create(:user)
-    login_as(@user, :scope => :user)
+    login_as(user, :scope => :user)
   end
 
   describe 'index' do
@@ -11,37 +17,37 @@ describe 'navigate' do
       visit posts_path
     end
 
-    it "has a title of Posts" do
-      expect(page).to have_content(/Posts/)
-    end
+  	it 'can be reached successfully' do
+  		expect(page.status_code).to eq(200)
+  	end
 
-    it "has a list of posts" do
+  	it 'has a title of Posts' do
+  		expect(page).to have_content(/Posts/)
+  	end
+
+    it 'has a list of posts' do
       post1 = FactoryGirl.create(:post)
       post2 = FactoryGirl.create(:second_post)
-      post1.update(user_id: @user.id)
-      post2.update(user_id: @user.id)
+      post1.update(user_id: user.id)
+      post2.update(user_id: user.id)
+
       visit posts_path
-      expect(page).to have_content(/Some|content/)
+
+      expect(page).to have_content(/Rationale|content/)
     end
 
     it 'has a scope so that only post creators can see their posts' do
-      post1 = Post.create(date: Date.today, rationale: "asdf",
-                              user_id: @user.id)
-      post2 = Post.create(date: Date.today, rationale: "asdf",
-                              user_id: @user.id)
-      other_user = User.create(first_name: "non", last_name: "user",
-                            email: "nonaut@email.com", password: "asdfasdf",
-                            password_confirmation: "asdfasdf")
-      posts_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen",
-                              user_id: other_user.id)
+      other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
+
       visit posts_path
 
       expect(page).to_not have_content(/This post shouldn't be seen/)
     end
   end
 
-  describe "new" do
-    it "has a link from the homepage" do
+  describe 'new' do
+    it 'has a link from the homepage' do
       visit root_path
 
       click_link("new_post_from_nav")
@@ -49,68 +55,65 @@ describe 'navigate' do
     end
   end
 
-  describe "delete" do
-    it "can be deleted" do
-      @post = FactoryGirl.create(:post)
-      @post.update(user_id: @user.id)
+  describe 'delete' do
+    it 'can be deleted' do
+      logout(:user)
+
+      delete_user = FactoryGirl.create(:user)
+      login_as(delete_user, :scope => :user )
+
+      post_to_delete = Post.create(date: Date.today, rationale: "asdf",
+                       user_id: delete_user.id)
       visit posts_path
 
-      click_link("delete_post_#{@post.id}_from_index")
+      click_link("delete_post_#{post_to_delete.id}_from_index")
       expect(page.status_code).to eq(200)
     end
   end
 
-  describe "creation" do
-    before do
-      visit new_post_path
-    end
-    it "has a new form that can be reached" do
-      expect(page.status_code).to eq(200)
-    end
+  describe 'creation' do
+  	before do
+  		visit new_post_path
+  	end
 
-    it "can be created from new form page" do
-      fill_in "post[date]", with: Date.today
-      fill_in "post[rationale]", with: "Some rationale"
+  	it 'has a new form that can be reached' do
+  		expect(page.status_code).to eq(200)
+  	end
+
+  	it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
-    end
+  	end
 
-    it "will have a user associated with it" do
-      fill_in "post[date]", with: Date.today
-      fill_in "post[rationale]", with: "User_Association"
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
       click_on "Save"
 
-      expect(User.last.posts.last.rationale).to eq("User_Association")
+      expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 
-  describe "edit" do
-    before do
-      @edit_user = User.create(first_name: "bob", last_name: "Muller",
-                   email: "bob@muller.com", password: "asdfasdf",
-                  password_confirmation: "asdfasdf" )
-      login_as(@edit_user, :scope => :user)
-      @edit_post = Post.create(date: Date.today, rationale: "asdf",
-                  user_id: @edit_user.id)
-    end
-
-    it "can be edited" do
-      visit edit_post_path(@edit_post)
+  describe 'edit' do
+    it 'can be edited' do
+      visit edit_post_path(post)
 
       fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "Edited Content"
+      fill_in 'post[rationale]', with: "Edited content"
       click_on "Save"
 
-      expect(page).to have_content("Edited Content")
+      expect(page).to have_content("Edited content")
     end
 
-    it "cannot be edited by a non authorized user" do
+    it 'cannot be edited by a non authorized user' do
       logout(:user)
       non_authorized_user = FactoryGirl.create(:non_authorized_user)
       login_as(non_authorized_user, :scope => :user)
 
-      visit edit_post_path(@edit_post)
+      visit edit_post_path(post)
 
       expect(current_path).to eq(root_path)
     end
