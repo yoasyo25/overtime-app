4e6a37eaf41c2aa111c0083494c02ff8f63c49b2
Yohanan Assefa <nanahoy@gmail.com>
2017-06-24 09:44:48 -0600

    Integrated phone validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :first_name, :last_name, :phone
+  PHONE_REGEX = /\A[0-9]*\z/
+  validates_format_of :phone, with: PHONE_REGEX
+  validates :phone, length: { is: 10 }
 
   def full_name
     "#{last_name}, #{first_name}".upcase
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,35 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "creation" do
-    before do
-      @user = FactoryGirl.create(:user)
-    end
+  before do
+    @user = FactoryGirl.create(:user)
+  end
 
+  describe "creation" do
     it "can be created" do
       expect(@user).to be_valid
     end
+  end
 
+  describe "validation" do
     it "cannot be created without first_name" do
       @user.first_name = nil
       expect(@user).to_not be_valid
     end
 
     it "cannot be created without last_name" do
-      @user.last_name  = nil
+      @user.last_name = nil
       expect(@user).to_not be_valid
     end
 
     it "cannot be created without a phone" do
-      @user.phone      = nil
+      @user.phone = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "requires the phone attr to only contain integers" do
+      @user.phone = 'mygreatstr'
+      expect(@user).to_not be_valid
+    end
+
+    it "requires the phone attr to only have 10 characters" do
+      @user.phone = '12345678901'
       expect(@user).to_not be_valid
     end
   end
 
-  end
   describe "custome name methods" do
     it "has a full name method that combines first and last name" do
-      @user = FactoryGirl.create(:user)
       expect(@user.full_name).to eq("SNOW, JOHN")
     end
+  end
 end
