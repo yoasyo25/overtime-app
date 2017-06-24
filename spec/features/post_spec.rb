6407340cf02487e2cfeb8d6662071c48411dcb5c
Yohanan Assefa <nanahoy@gmail.com>
2017-06-23 20:10:58 -0600

    Added hours requested to post model

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,7 +4,8 @@ describe 'navigate' do
   let(:user) { FactoryGirl.create(:user) }
 
   let(:post) do
-    Post.create(date: Date.today, rationale: "rationale", user_id: user.id)
+    Post.create(date: Date.today, rationale: "rationale", user_id: user.id,
+    overtime_request: 3.5)
   end
 
   before do
@@ -37,7 +38,7 @@ describe 'navigate' do
 
     it 'has a scope so that only post creators can see their posts' do
       other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
-      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id, overtime_request: 3.5)
 
       visit posts_path
 
@@ -62,7 +63,7 @@ describe 'navigate' do
       login_as(delete_user, :scope => :user )
 
       post_to_delete = Post.create(date: Date.today, rationale: "asdf",
-                       user_id: delete_user.id)
+                       user_id: delete_user.id, overtime_request: 3.5)
       visit posts_path
 
       click_link("delete_post_#{post_to_delete.id}_from_index")
@@ -82,14 +83,15 @@ describe 'navigate' do
   	it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
-      click_on "Save"
+      fill_in 'post[overtime_request]', with: 4.5
 
-      expect(page).to have_content("Some rationale")
+      expect { click_on "Save"}.to change(Post, :count).by(1)
   	end
 
     it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "User Association"
+      fill_in 'post[overtime_request]', with: 4.5
       click_on "Save"
 
       expect(User.last.posts.last.rationale).to eq("User Association")
